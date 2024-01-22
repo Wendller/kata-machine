c181813e805fb00c56052586432e45b6d08db3d8
Wendler Tenorio <wendler_mendes@hotmail.com>
2024-01-22 20:53:05 -0300

    feat: binary search implementation

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let lowerPoint = 0;
+    let higherPoint = haystack.length;
 
+    do {
+        const middle = Math.floor(lowerPoint + (higherPoint - lowerPoint) / 2);
+        const value = haystack[middle];
+
+        if (value === needle) {
+            return true;
+        } else if (value > needle) {
+            higherPoint = middle;
+        } else {
+            lowerPoint = middle + 1;
+        }
+    } while (lowerPoint < higherPoint);
+
+    return false;
 }
